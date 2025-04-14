3ee07df30b3fcd992a4aaaddab9ac7e38cd07ec7
glob-bruh <34488272+glob-bruh@users.noreply.github.com>
2025-04-13 20:00:12 -0400

    update 365 script.

--- Pro365er/Pro356er.ps1
+++ Pro365er/Pro356er.ps1
@@ -4,6 +4,33 @@
 # GLOBBRUH MS ENTRA MANAGER #
 #############################
 
+function lineBreaks ($style) {
+    switch ($style) {
+        "#"   {Write-Output "================================================================"}
+        "##"  {Write-Output "----------------------------------------------------------------"}
+        "###" {Write-Output "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - "}
+    }
+}
+
+function searchParser ($inVar) {
+    $txtPfx = "[SEARCH]"
+    $term = Read-Host "$txtPfx Enter search term"
+    lineBreaks "###" ; Write-Output "SEARCH RESULTS:"
+    $itemInLine = 0 ; $targStr = ""
+    foreach ($x in $inVar) {
+        if ($x -like "*$term*") {
+            if ($itemInLine -eq 2) {
+                Write-Output ($targStr + $x)
+                $targStr = "" ; $itemInLine = 0
+            } else {
+                $targStr += "$x [] " ; $itemInLine += 1
+            }
+        }
+    }
+    Write-Output $targStr
+    lineBreaks "###"
+}
+
 function passwordGenerator () {
     $txtPfx = "[PASS GEN]" 
     $length = [int](Read-Host "$txtPfx Length")
@@ -12,11 +39,19 @@ function passwordGenerator () {
     return [System.Web.Security.Membership]::GeneratePassword($length, $symbNum)
 }
 
-function checkImportGraph () {
-    if ("Microsoft.Graph.Users" -notin (Get-Module).Name) {
+function checkImportGraph ($skipCheck) {
+    if (("Microsoft.Graph.Users" -notin (Get-Module).Name) -or ($skipCheck -eq $true)) {
+        if (-not (Get-Module -ListAvailable -Name "Microsoft.Graph")) {
+            Write-Output "MS Graph is not installed! It's required to run this script!"
+            $sel = Read-Host "Install MS Graph (Y/n)" ; if ($sel -like "y") {
+                Write-Output "Installing MS Graph..." 
+                Install-Module -Name "Microsoft.Graph"
+                Write-Output "MS Graph installed."
+            }
+        }
         Write-Output "MS Graph not imported. Importing..."
         $maximumfunctioncount = "32768"
-        Import-Module -Name Microsoft.Graph
+        Import-Module -Name Microsoft.Graph -Force
         Write-Output "MS Graph Imported!"
     }
 }
@@ -27,9 +62,10 @@ function connectGraph () {
     Write-Output "2) Authenticate with SECRET (requires SECRET.json)"
     $sel = Read-Host "Choice"
     Write-Output "Connect to tenant..."
+    $scopes = "User.ReadWrite.All", "Group.ReadWrite.All", "Domain.ReadWrite.All"
     switch ($sel) {
         "1" {
-            Connect-MgGraph -Scopes "User.Read.All", "Group.ReadWrite.All" -NoWelcome -ErrorAction SilentlyContinue
+            Connect-MgGraph -Scopes $scopes -NoWelcome -ErrorAction SilentlyContinue
             if ($?) { Write-Output "CONNECTED TO TENANT!" } else { Write-Output "CONNECT FAILED!" }
         }
         "2" {
@@ -43,8 +79,12 @@ function connectGraph () {
 }
 
 function userManageFunc () {
-    Write-Output "", "---", "USERS:", "---"
-    Write-Output (Get-MgUser).DisplayName, "---"
+    Write-Output "Get user list..."
+    $usersList = Get-MgUser
+    Write-Output "" ; lineBreaks "##" ; Write-Output "USERS:" ; lineBreaks "##"
+    Write-Output $usersList.DisplayName ; lineBreaks "##"
+    Write-Output "Please enter the name of the user you want to manage or select an option:"
+    Write-Output "SEARCH will let you search for a user."
     Write-Output "1) Add User", "2) User License Managment", "3) Delete User", "4) Exit"
     $sel = Read-Host "Choice" ; switch ($sel) {
         "1" {
@@ -75,37 +115,50 @@ function userManageFunc () {
                 Write-Output "New user created (ID: $($out.Id))."
             }
         }
+        "SEARCH" {searchParser $usersList.DisplayName}
+        default {
+            if ($sel -in $usersList.DisplayName) {
+                $userInfo = ($usersList | Where-Object {$_.DisplayName -eq $sel})
+                lineBreaks "##"
+                Write-Output "USER INFORMATION:", "NAME - $($userInfo.DisplayName)", "ID - $($userInfo.Id)" `
+                    "UPN - $($userInfo.UserPrincipalName)", "ABOUT - $($userInfo.AboutMe)" `
+                    "LICENSES - $($userInfo.AssignedLicenses)", "ACC ENABLED - $($userInfo.AccountEnabled)"
+                lineBreaks "##"
+                Write-Output "1) Manage License", "2) Disable User", "3) Exit"
+            }
+        }
     }
 }
 
 function shutdownAndExitTenant () {
     Write-Output "Disconnect from tenant..."
-    Disconnect-Graph
+    $out = Disconnect-Graph
+    Write-Output "Disconnected from tenant $($out.TenantId)."
 }
 
-Write-Output "-----------------------------------------"
+lineBreaks "#"
 Write-Output "PRO           GLOBBRUH CLOUD PROJECT"
 Write-Output "    365"
 Write-Output "        ER"
-Write-Output "-----------------------------------------"
+lineBreaks "#"
 Write-Output "WARNING:"
 Write-Output "Please read license before using!"
-Write-Output "-----------------------------------------"
+lineBreaks "#"
 
-checkImportGraph
+checkImportGraph $false
 connectGraph
 
-while ($curActive -ne 0) {
+$curActive = 1 ; while ($curActive -ne 0) {
     $name = (Get-MgContext).Account
     if ( $name -eq $null ) { $name = "User" }
-    Write-Output "", "=== WELCOME $name! ==="
-    Write-Output "1) User Managment"
-    Write-Output "2) EXIT"
+    Write-Output "=== WELCOME $name! ==="
+    Write-Output "1) User Managment", "2) Force-reimport of MSGraph", "3) EXIT"
     Write-Output "=========="
     $sel = Read-Host "Choice"
     switch ($sel) {
-        "1" { userManageFunc }
-        "2" { shutdownAndExitTenant ; $curActive = 0 }
+        1 { userManageFunc }
+        2 { checkImportGraph $true }
+        3 { shutdownAndExitTenant ; $curActive = 0 }
         default { Write-Output "Not a valid selection." }
     }
 }
