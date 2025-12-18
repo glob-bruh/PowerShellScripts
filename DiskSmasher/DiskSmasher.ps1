5d16ad30cf709be9594c00ef25a35b0432edd0ee
glob-bruh <34488272+glob-bruh@users.noreply.github.com>
2025-12-17 21:00:03 -0700

    add ability to gather total size based off a publisher on a signed file.

--- DiskSmasher/DiskSmasher.ps1
+++ DiskSmasher/DiskSmasher.ps1
@@ -7,6 +7,9 @@
 # Created by GlobBruh
 ######################
 
+[CmdletBinding()]
+param()
+
 function numRound ($inVar) {
     return [math]::Round($inVar, 2)
 }
@@ -18,7 +21,7 @@ function getBasicInformation () {
     $upTm = (Get-Date) - $x.LastBootUpTime
     $upTmS = $upTm.Seconds ; $upTmM = $upTm.Minutes ; $upTmH = $upTm.Hours  ; $upTmD = $upTm.Days
     Write-Output "-> General Information:"
-    Write-Output "--> Current Domain/User: $curUser. [] Uptime: S:$upTmS M:$upTmM H:$upTmH D:$upTmD."
+    Write-Output "--> Current Domain/User: $curUser. || Uptime: S:$upTmS M:$upTmM H:$upTmH D:$upTmD."
     $x = Get-PSDrive -Name "C"
     $usedSpace =  numRound ($x.Used / 1gb)
     $freeSpace =  numRound ($x.Free / 1gb)
@@ -26,14 +29,14 @@ function getBasicInformation () {
     $percUsed = numRound (($usedSpace / $totalSpace) * 100)
     $percFree = numRound (($freeSpace / $totalSpace) * 100)
     Write-Output "-> Disk Usage:"
-    Write-Output "--> Total (gb): $totalSpace. [] Used/Free (gb): $usedSpace/$freeSpace. [] Used/Free (%): $percUsed/$percFree."
+    Write-Output "--> Total (gb): $totalSpace. || Used/Free (gb): $usedSpace/$freeSpace. || Used/Free (%): $percUsed/$percFree."
 }
 
 function getSizeOfFolderContents ($path) {
     Write-Output "-> Retrieve total size of $path`:"
     $i = 0
     if ((Test-Path -Path $path) -eq $true) {
-        foreach ($x in (Get-ChildItem -Path $path -Recurse -File -ErrorAction SilentlyContinue)) {
+        foreach ($x in (Get-ChildItem -Path $path -Recurse -File -Force -ErrorAction SilentlyContinue)) {
             $i += $x.Length
         }
         $out = numRound ($i / 1gb)
@@ -45,7 +48,7 @@ function getSizeOfFolderContents ($path) {
 
 function listLargeFilesInFolders ($path, $fileCount) {
     Write-Output "-> Largest files in $path (top $fileCount):"
-    $x = (Get-ChildItem -Path $path -Recurse -File -ErrorAction SilentlyContinue | Sort-Object Length | Select-Object -Last $fileCount)
+    $x = (Get-ChildItem -Path $path -Recurse -File -Force -ErrorAction SilentlyContinue | Sort-Object Length | Select-Object -Last $fileCount)
     foreach ($i in $x) {
         $size = numRound ($i.Length / 1gb)
         Write-Output "--> $size gb: $($i.FullName)"
@@ -68,23 +71,40 @@ function getUserFolderStats () {
     listLargeFilesInFolders "C:\Users\" 12
 }
 
+function getSizeOfAllSignedFilesInDir($path, $subject) {
+    $totalLen = 0
+    $orgName = $subject.split(",")[2].split("=")[1]
+    Write-Output "-> Check $path for files with specific signature from $orgName"
+    $x = Get-ChildItem -Path $path -File -Force 
+    foreach ($i in $x) {
+        if ((Get-AuthenticodeSignature -FilePath $i.FullName).SignerCertificate.Subject -eq $subject) {
+            $totalLen += $i.Length
+            Write-Verbose "---> $i`: $(numRound($i.Length / 1gb)) gb"
+        }
+    }
+    Write-Output "--> Total size of signed files in this folder: $(numRound($totalLen / 1gb)) gb."
+}
+
 function checkNoteableLargeFolders () {
     getSizeOfFolderContents "C:\Windows\CSC\"
     getSizeOfFolderContents "C:\Windows\SoftwareDistribution\Download\"
+    $sigSubject = "CN=Adobe Inc., OU=Acrobat DC, O=Adobe Inc., L=San Jose, S=ca, C=US, SERIALNUMBER=2748129, OID.2.5.4.15=Private Organization, OID.1.3.6.1.4.1.311.60.2.1.2=Delaware, OID.1.3.6.1.4.1.311.60.2.1.3=US"
+    getSizeOfAllSignedFilesInDir "C:\Windows\Installer\" $sigSubject
 }
 
 function newSection ($sectionTxt) {
-    Write-Output "==============", ($sectionTxt + ":")
+    Write-Output "==============================", ($sectionTxt + ":")
 }
 
-Write-Output "=============="
-Write-Output "DISK SMASHER"
+Write-Output "=============================="
+Write-Output "        DISK SMASHER"
+Write-Output "GlobBruh @ tech.beyondgone.xyz"
 newSection "BASIC INFORMATION"
 getBasicInformation
 newSection "STATS - ROOT OF VOLUME"
 getVolumeRootStats
 newSection "STATS - USER FOLDER"
 getUserFolderStats
-newSection "NOTABLE LARGE FOLDERS"
+newSection "CHECK KNOWN PROBLEMATIC LOCATIONS"
 checkNoteableLargeFolders
 # checkForRemediation
--- DiskSmasher/README.md
+++ DiskSmasher/README.md
@@ -1,6 +1,6 @@
 # DiskSmasher
 
-## Open-Source implementation of a disk scanning script. 
+## Open-Source implementation of scanning script for visualizing drive consumption. 
 
 ## Script origins:
 This script is an open-source version of a proprietary disk scanning script I built. It has also been written from scratch and does not modify settings (only scans). 
